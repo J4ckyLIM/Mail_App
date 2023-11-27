3e3c235ce735aa0ce2e31a8dff893f8da1bb791c
J4ckyLIM <j4cky.lm@gmail.com>
2023-11-27 11:54:49 +0100

    feat: message entity and relation

---
+++ backend/src/domain/message/message.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, ManyToOne } from "typeorm";
+import { BaseEntity, BaseEntityCreateArgs } from "../base.entity";
+import { User } from "../user/user.entity";
+
+interface MessageCreateArgs extends BaseEntityCreateArgs {
+  title: string;
+  content: string;
+  writtenBy: User;
+  writtenTo: User;
+}
+
+@Entity()
+export class Message extends BaseEntity {
+  @Column()
+  title: string;
+  
+  @Column({ type: 'longtext' })
+  content: string;
+
+  @Column({ default: false })
+  hasBeenRead: boolean;
+
+  @ManyToOne(() => User, (user) => user.messagesWritten)
+  writtenBy: User;
+
+  @ManyToOne(() => User, (user) => user.messagesReceived)
+  writtenTo: User;
+
+  constructor(props: MessageCreateArgs) {
+    super(props);
+    this.title = props.title;
+    this.content = props.content;
+    this.writtenBy = props.writtenBy;
+    this.writtenTo = props.writtenTo;
+    this.hasBeenRead = false;
+  }
+}
