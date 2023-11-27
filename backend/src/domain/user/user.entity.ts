3229ec7639cb146665cc254947375b7611ddb952
J4ckyLIM <j4cky.lm@gmail.com>
2023-11-27 09:36:56 +0100

    feat: user entity

--- backend/src/domain/user/user.entity.ts
+++ backend/src/domain/user/user.entity.ts
@@ -4,15 +4,25 @@ import { BaseEntity, BaseEntityCreateArgs } from '../base.entity';
 
 export interface UserCreateArgs extends BaseEntityCreateArgs {
   name: string;
+  email: string;
+  password: string;
 }
 
 @Entity()
 export class User extends BaseEntity {
   @Column()
   name: string;
+  
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   constructor(props: UserCreateArgs) {
     super(props);
     this.name = props.name;
+    this.email = props.email;
+    this.password = props.password;
   }
 }
