54340a140fd9c82b3e08a931e3170b6f4d9cf3a6
J4ckyLIM <j4cky.lm@gmail.com>
2023-11-27 17:17:17 +0100

    feat: e2e test for auth controller

---
+++ backend/test/auth.controller.e2e-spec.ts
@@ -0,0 +1,55 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AuthService } from '../src/services/auth/auth.service';
+import { AuthController } from '../src/controllers/auth/auth.controller';
+import { LocalAuthGuard } from '../src/guard/local-auth.guard';
+import { UserService } from '../src/services/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../src/domain/user/user.entity';
+
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [AuthService, UserService, JwtService, {
+        provide: getRepositoryToken(User),
+        useValue: {
+          findOneBy: jest.fn(),
+        }
+      }],
+    })
+      .overrideGuard(LocalAuthGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    authService = moduleFixture.get<AuthService>(AuthService);
+
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  describe('/auth/login (POST)', () => {
+    it('should return the user token on successful login', async () => {
+      const user = { username: 'testuser', password: 'testpassword' };
+      const token = { access_token: 'testtoken' };
+
+      jest.spyOn(authService, 'login').mockResolvedValue(token);
+
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(user)
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toEqual(token);
+    });
+  });
+});
