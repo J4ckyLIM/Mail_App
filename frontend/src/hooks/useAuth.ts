e12b6a9428be154b399d1599787bca6369e2623d
J4ckyLIM <j4cky.lm@gmail.com>
2023-12-01 11:33:34 +0100

    feat: useAuth hook

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,11 @@
+import { useContext } from "react"
+import { AuthenticationContext } from "../contexts/AuthenticationProvider"
+
+export const useAuth = () => {
+  const { accessToken, login, register } = useContext(AuthenticationContext);
+  return {
+    accessToken,
+    login,
+    register,
+  };
+}
