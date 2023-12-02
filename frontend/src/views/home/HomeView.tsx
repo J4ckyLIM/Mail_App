49aa841859125386d417618f05a77c60f05d99e8
J4ckyLIM <j4cky.lm@gmail.com>
2023-12-02 21:38:26 +0100

    wip: main view

--- frontend/src/views/home/HomeView.tsx
+++ frontend/src/views/home/HomeView.tsx
@@ -1,15 +1,49 @@
-import { Text, VStack } from '@chakra-ui/react';
-import { FC } from 'react';
+import { Box, Checkbox, HStack, Text, VStack } from '@chakra-ui/react';
+import { FC, useMemo, useState } from 'react';
 
-import { Page } from '../../components/layouts/page/Page';
+import { EmptyMailIcon } from '../../assets';
+import { useGetAllReceivedMessage } from '../../api/messages';
 
 const HomeView: FC = () => {
+  const [displayOnlyUnread, setDisplayOnlyUnread] = useState<boolean>(false);
+
+  const { isLoading, isError, messages } = useGetAllReceivedMessage();
+
+  const filteredMessages = useMemo(() => {
+    if (displayOnlyUnread) {
+      return messages?.filter(message => !message.hasBeenRead);
+    }
+    return messages;
+  }, [messages, displayOnlyUnread]);
+
+  console.log(filteredMessages);
+
   return (
-    <Page title="Home">
-      <VStack>
-        <Text>Home view</Text>
+    <Box border="1px solid green" w="full" h="full" display="flex">
+      <VStack flex="34%" alignItems="flex-start" h="full">
+        <HStack w="full" justifyContent="space-between" p="2" borderBottom="1px solid gray" borderColor="gray.200">
+          <Text fontWeight="semibold" fontSize="sm">
+            Boîte de réception ({filteredMessages?.length ?? 0})
+          </Text>
+          <Checkbox size='md' colorScheme='red' isChecked={displayOnlyUnread} onChange={(event) => setDisplayOnlyUnread(event.target.checked)}>
+            <Text fontSize="xs" color="gray" fontWeight="normal">
+              Afficher uniquement les messages non lus
+            </Text>
+          </Checkbox>
+        </HStack>
+        <Text>Mail</Text>
       </VStack>
-    </Page>
+      <HStack
+        flex="66%"
+        height="100%"
+        w="full"
+        border="1px solid blue"
+        backgroundColor="#F1F1F1"
+        justifyContent="center"
+      >
+        <img src={EmptyMailIcon} />
+      </HStack>
+    </Box>
   );
 };
 
