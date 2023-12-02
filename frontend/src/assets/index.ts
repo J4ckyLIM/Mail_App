fa4cebe5badb754825d165a736c0554a28f1d512
J4ckyLIM <j4cky.lm@gmail.com>
2023-12-02 12:13:16 +0100

    feat: logout icon

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,5 +1,6 @@
 import KeyIcon from './key.svg';
 import LogoIcon from './logo.svg';
 import PersonIcon from './person.svg';
+import LogoutIcon from './logout.svg';
 
-export { PersonIcon, KeyIcon, LogoIcon };
+export { PersonIcon, KeyIcon, LogoIcon, LogoutIcon };
