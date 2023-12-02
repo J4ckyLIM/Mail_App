114dc90b4581640e167649b673e8542ef4096561
J4ckyLIM <j4cky.lm@gmail.com>
2023-12-02 21:37:10 +0100

    feat: add empty mail icon

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -2,5 +2,6 @@ import KeyIcon from './key.svg';
 import LogoIcon from './logo.svg';
 import PersonIcon from './person.svg';
 import LogoutIcon from './logout.svg';
+import EmptyMailIcon from './emptyMail.svg';
 
-export { PersonIcon, KeyIcon, LogoIcon, LogoutIcon };
+export { PersonIcon, KeyIcon, LogoIcon, LogoutIcon, EmptyMailIcon };
