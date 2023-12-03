ed15b5f3e6b4da0ebe3fdff36bc739077eb2a825
J4ckyLIM <j4cky.lm@gmail.com>
2023-12-03 11:57:31 +0100

    feat: scrollable list component

---
+++ frontend/src/components/lists/ScrollableList.tsx
@@ -0,0 +1,21 @@
+
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface ScrollableListProps<T> {
+  items: T[];
+  renderItem: (item: T) => ReactNode;
+}
+
+const ScrollableList = <T extends Object>({
+  items,
+  renderItem,
+}: ScrollableListProps<T>) => {
+  return (
+    <Box h="full" overflowY="auto" w="full">
+      {items.map((item) => renderItem(item))}
+    </Box>
+  );
+};
+
+export default ScrollableList;
